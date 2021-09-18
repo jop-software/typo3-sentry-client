3f0774a67b98d804b22585c458958a72a9705595
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-09-18 07:42:32 +0200

    [TASK] Add ext_emconf and ext_icon files

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Sentry Client for TYPO3',
+	'description' => 'Use Sentry to track errors in your TYPO3-installation',
+	'category' => 'misc',
+	'author' => 'jop-software Inh. Johannes Przymusinski',
+	'author_email' => 'info@jop-software.de',
+	'author_company' => 'jop-software Inh. Johannes Przymusinski',
+	'state' => 'in-dev',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearCacheOnLoad' => true,
+	'version' => '0.1.0',
+	'constraints' => [
+		'depends' => ['typo3' => '11.4.0-11.99.99'],
+		'conflicts' => [],
+		'suggests' => []
+	]
+];
