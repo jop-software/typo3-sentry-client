8f9e17969c182389963bb0abe07a00c883d66384
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-09-19 10:53:26 +0200

    [TASK] Add basic TypoScript for the demo_extension

---
+++ Tests/Packages/demo_extension/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	"demo_extension",
+	"Configuration/TypoScript/",
+	"demo_extension"
+);
