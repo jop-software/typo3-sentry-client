33aafbc7955524cc865ff41d72774b3fbbddd04f
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-10-30 14:08:49 +0200

    [TASK] Attatch a logger to the SentryClient

--- Classes/Service/SentryService.php
+++ Classes/Service/SentryService.php
@@ -4,6 +4,8 @@ namespace Jops\TYPO3\Sentry\Service;
 
 use Sentry\ClientBuilder;
 use Sentry\SentrySdk;
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class SentryService
 {
@@ -18,6 +20,13 @@ class SentryService
 			"release" => ConfigurationService::getRelease(),
 			"environment" => ConfigurationService::getEnvironment()
 		]);
+
+		// We need to use the GeneralUtility to instantiate a LogManager, because we can't use either
+		// DependencyInjection or the LoggerAwareTrait.
+		$clientBuilder->setLogger(
+			GeneralUtility::makeInstance(LogManager::class)
+				->getLogger("Sentry-Client") // TODO: The name of the logger should be configurable
+		);
 
 		SentrySdk::init()->bindClient($clientBuilder->getClient());
 	}
