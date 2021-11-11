99a8f0274b22144a98820b069742ce1e0fa2d6d4
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-11-11 20:26:24 +0100

    [TASK] Use a TYPO3 request object to build the transaction name

--- Classes/Bootstrap/BootCompletedListener.php
+++ Classes/Bootstrap/BootCompletedListener.php
@@ -4,15 +4,25 @@ namespace Jops\TYPO3\Sentry\Bootstrap;
 
 use Jops\TYPO3\Sentry\Service\SentryService;
 use TYPO3\CMS\Core\Core\Event\BootCompletedEvent;
+use TYPO3\CMS\Core\Http\ServerRequestFactory;
 
 class BootCompletedListener
 {
 	public function __invoke(BootCompletedEvent $event): void
 	{
+		// We need to create our own request object because it seems to be that there is no way to get this
+		// information from TYPO3 at this point of the boot process :-)
+		$request = ServerRequestFactory::fromGlobals();
+
 		SentryService::initialize();
-		SentryService::startTransaction(sprintf("%s %s",
-			$_SERVER["REQUEST_METHOD"],
-			$_SERVER["SCRIPT_URI"],
+		SentryService::startTransaction(sprintf("%s %s://%s%s%s",
+			$request->getMethod(),
+			$request->getUri()->getScheme(),
+			$request->getUri()->getHost(),
+			$request->getUri()->getPath(),
+			$request->getUri()->getQuery()
+				? "?{$request->getUri()->getQuery()}"
+				: "",
 		));
 	}
 }
