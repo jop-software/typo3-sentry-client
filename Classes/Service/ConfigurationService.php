5b550efb8efb90c88226b600d4b01b5930800139
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-11-15 22:43:11 +0100

    [TASK] Add plugin configuration

--- Classes/Service/ConfigurationService.php
+++ Classes/Service/ConfigurationService.php
@@ -2,25 +2,78 @@
 
 namespace Jops\TYPO3\Sentry\Service;
 
+use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
+use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class ConfigurationService
 {
+	/**
+	 * Get the value from extension configuration by the path.
+	 *
+	 * @param string $path
+	 * @return string
+	 * @throws ExtensionConfigurationExtensionNotConfiguredException
+	 * @throws ExtensionConfigurationPathDoesNotExistException
+	 */
+	private static function getExtensionConfiguration(string $path): string
+	{
+		return strval(GeneralUtility::makeInstance(ExtensionConfiguration::class)
+			->get("typo3_sentry_client", $path));
+	}
+
 	public static function getDsn(): string
 	{
-		return getenv("SENTRY_DSN") ?: "";
+		if ($dsn = getenv("SENTRY_DSN")) {
+			return strval($dsn);
+		}
+
+		if ($dsn = self::getExtensionConfiguration("dsn")) {
+			return $dsn;
+		}
+
+		// TODO: we should throw an exception here instead of just returning an empty string because its required
+		//  by the sentry SDK
+		return "";
 	}
 
 	public static function getRelease(): string
 	{
-		return getenv("SENTRY_RELEASE") ?: "";
+		if ($dsn = getenv("SENTRY_RELEASE")) {
+			return strval($dsn);
+		}
+
+		if ($dsn = self::getExtensionConfiguration("release")) {
+			return $dsn;
+		}
+
+		return "";
 	}
 
 	public static function getEnvironment(): string
 	{
-		return getenv("SENTRY_ENVIRONMENT") ?: "";
+		if ($dsn = getenv("SENTRY_ENVIRONMENT")) {
+			return strval($dsn);
+		}
+
+		if ($dsn = self::getExtensionConfiguration("environment")) {
+			return $dsn;
+		}
+
+		return "";
 	}
 
 	public static function getTracesSampleRate(): float
 	{
-		return floatval(getenv("SENTRY_TRACES_SAMPLE_RATE")) ?: 0.5;
+		if ($dsn = getenv("SENTRY_TRACES_SAMPLE_RATE")) {
+			return strval($dsn);
+		}
+
+		if ($dsn = self::getExtensionConfiguration("traces_sample_rate")) {
+			return floatval($dsn);
+		}
+
+		return 0.5;
 	}
 }
