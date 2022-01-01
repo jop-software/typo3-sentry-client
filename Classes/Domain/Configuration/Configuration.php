88cce6640dfa069d08651f8d5d4a280abf08afb0
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-01-01 18:17:26 +0100

    feat: very basic domain configuration class

---
+++ Classes/Domain/Configuration/Configuration.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Jops\TYPO3\Sentry\Domain\Configuration;
+
+use Exception;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+
+class Configuration
+{
+
+    protected ExtensionConfiguration $extensionConfiguration;
+
+    public function __construct(
+        ExtensionConfiguration $extensionConfiguration
+    ) {
+        $this->extensionConfiguration = $extensionConfiguration;
+
+        try {
+            $configuration = $this->extensionConfiguration->get("typo3_sentry_client");
+
+            foreach ($configuration as $key => $value) {
+                if (property_exists(__CLASS__, $key)) {
+                    $this->$key = $value;
+                }
+            }
+        } catch (Exception $exception) {  /* TODO: Better error handling */ }
+
+    }
+
+    protected bool $active = false;
+    protected string $dsn = "";
+    protected string $environment = "Production";
+    protected string $release = "1.0.0";
+    protected float $traces_sample_rate = 0.0;
+
+    /**
+     * @return ExtensionConfiguration
+     */
+    public function getExtensionConfiguration(): ExtensionConfiguration
+    {
+        return $this->extensionConfiguration;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDsn(): string
+    {
+        return $this->dsn;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEnvironment(): string
+    {
+        return $this->environment;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRelease(): string
+    {
+        return $this->release;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTracesSampleRate(): float
+    {
+        return $this->traces_sample_rate;
+    }
+}
