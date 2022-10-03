4972040a20f2155d4e144f702bc102b404dc840c
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-10-03 23:06:25 +0200

    feat: filter every handled exceptions through a blacklist filter

--- Classes/Handler/DebugExceptionHandler.php
+++ Classes/Handler/DebugExceptionHandler.php
@@ -3,6 +3,7 @@
 namespace Jops\TYPO3\Sentry\Handler;
 
 use Jops\TYPO3\Sentry\Domain\Configuration\Configuration;
+use Jops\TYPO3\Sentry\Service\Blacklist;
 use Jops\TYPO3\Sentry\Service\SentryService;
 use Throwable;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -14,6 +15,12 @@ class DebugExceptionHandler extends \TYPO3\CMS\Core\Error\DebugExceptionHandler
     public function handleException(Throwable $exception): void
     {
         $configuration = GeneralUtility::makeInstance(Configuration::class);
+
+        // Check weather the exception is excluded
+        if (Blacklist::isExcluded(get_class($exception))) {
+            parent::handleException($exception);
+            return;
+        }
 
         $dsn = $configuration->getDsn();
         if ($dsn === '' || $dsn === '0') {
